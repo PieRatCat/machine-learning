e309e797c118dcccf36821bbe933a2322b6c7bbd
PieRatCat <AmsSumner@gmail.com>
2025-03-11 20:56:21 +0100

    streamlit

--- kk2/streamlit/page_1.py
+++ kk2/streamlit/page_1.py
@@ -1,1 +1,11 @@
 import streamlit as st
+
+st.markdown("# This is the main page where the content will go")
+
+if "counter" not in st.session_state:
+    st.session_state.counter = 0
+
+st.session_state.counter += 1
+
+st.header(f"This page has run {st.session_state.counter} times.")
+st.button("Run it again")
