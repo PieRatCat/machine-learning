1d13016dccf50bcc08c138090517138acdc77fb7
PieRatCat <AmsSumner@gmail.com>
2025-03-15 22:52:19 +0100

    main app

--- kk2/streamlit/streamlit_app.py
+++ kk2/streamlit/streamlit_app.py
@@ -1,17 +1,5 @@
 import streamlit as st
 
-st.markdown(
-    """
-    <style>
-    html, body {
-        font-size: 18px;            
-    }
-    a {font-size: 20px;}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 # Define the pages
 page_1 = st.Page("page_1.py", title="About project", icon=":material/home:")
 page_2 = st.Page("page_2.py", title="App demo", icon=":material/draw:")
